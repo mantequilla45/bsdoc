72adacb43da6e31ea1d336515edeadc808b04126
Vince Kevin Añana <152976699+mantequilla45@users.noreply.github.com>
2025-02-03 13:36:50 -0500

    update

--- src/app/(pages)/search-symptoms/page.tsx
+++ src/app/(pages)/search-symptoms/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Footer from "@/app/components/layout/footer";
 import { motion, AnimatePresence } from "framer-motion";
 import AdvancedSearchForm from "@/app/components/search-symptoms/adv-search-form";
+import ImageContainer from "@/app/components/search-symptoms/background";
 
 const SearchSymptomsPage = () => {
     const [isAdvancedSearchEnabled, setIsAdvancedSearchEnabled] = useState(false);
@@ -53,90 +54,11 @@ const SearchSymptomsPage = () => {
                 <title>Search Symptoms</title>
 
                 {/* Container for images */}
-                <div className="absolute inset-0 overflow-hidden pointer-events-none">
-                    {/* Fixed images for scrolled state */}
-                    {isSticky && isScrolled && (
-                        <div className="fixed inset-0">
-                            <Image
-                                alt="Search Page"
-                                className="h-[800px] w-[1500px] right-0 bottom-0"
-                                style={{
-                                    position: 'fixed',
-                                    color: 'transparent'
-                                }}
-                                src="/graphics/searchpage.svg"
-                                width={0}
-                                height={0}
-                            />
-                            <Image
-                                alt="Search Page"
-                                className="h-[700px] w-[1350px] right-0 bottom-5"
-                                style={{
-                                    position: 'fixed',
-                                    color: 'transparent'
-                                }}
-                                src="/graphics/searchpageicon.svg"
-                                width={0}
-                                height={0}
-                            />
-                        </div>
-                    )}
-
-                    {/* Absolute images for non-scrolled or footer state */}
-                    {(!isSticky || !isScrolled) && (
-                        <div className="absolute inset-0 overflow-hidden pointer-events-none">
-                            <div className="sticky top-0 w-full h-full">
-                                <Image
-                                    alt="Search Page"
-                                    className="absolute h-[800px] w-[1500px] right-0 bottom-0"
-                                    style={{
-                                        color: 'transparent'
-                                    }}
-                                    src="/graphics/searchpage.svg"
-                                    width={0}
-                                    height={0}
-                                />
-                                <Image
-                                    alt="Search Page"
-                                    className="absolute h-[700px] w-[1350px] right-0 bottom-5"
-                                    style={{
-                                        color: 'transparent',
-                                    }}
-                                    src="/graphics/searchpageicon.svg"
-                                    width={0}
-                                    height={0}
-                                />
-                            </div>
-                        </div>
-                    )}
-
-                    {!isScrolled && (
-                        <div className="absolute inset-0 overflow-hidden pointer-events-none">
-                            <div className="sticky top-0 w-full h-full">
-                                <Image
-                                    alt="Search Page"
-                                    className="fixed h-[800px] w-[1500px] right-0 bottom-0"
-                                    style={{
-                                        color: 'transparent'
-                                    }}
-                                    src="/graphics/searchpage.svg"
-                                    width={0}
-                                    height={0}
-                                />
-                                <Image
-                                    alt="Search Page"
-                                    className="fixed h-[700px] w-[1350px] right-0 bottom-5"
-                                    style={{
-                                        color: 'transparent',
-                                    }}
-                                    src="/graphics/searchpageicon.svg"
-                                    width={0}
-                                    height={0}
-                                />
-                            </div>
-                        </div>
-                    )}
-                </div>
+                <ImageContainer
+                    isSticky={isSticky}
+                    isScrolled={isScrolled}
+                    isAdvancedSearchEnabled={isAdvancedSearchEnabled}
+                />
 
 
                 <div className="flex z-10 flex-col gap-5 mt-16 relative">
---
+++ src/app/components/search-symptoms/background.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface ImageContainerProps {
+    isSticky: boolean;
+    isScrolled: boolean;
+    isAdvancedSearchEnabled: boolean;
+}
+
+const ImageContainer: React.FC<ImageContainerProps> = ({
+    isSticky,
+    isScrolled,
+    isAdvancedSearchEnabled
+}) => {
+
+    return (
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+            {/* Fixed images for scrolled state */}
+            {isSticky && isScrolled && (
+                <div className="fixed inset-0">
+                    <Image
+                        alt="Search Page"
+                        className="h-[800px] w-[1500px] right-0 bottom-0"
+                        style={{
+                            position: 'fixed',
+                            color: 'transparent'
+                        }}
+                        src="/graphics/searchpage.svg"
+                        width={0}
+                        height={0}
+                    />
+                    <Image
+                        alt="Search Page"
+                        className="h-[700px] w-[1350px] right-0 bottom-5"
+                        style={{
+                            position: 'fixed',
+                            color: 'transparent'
+                        }}
+                        src="/graphics/searchpageicon.svg"
+                        width={0}
+                        height={0}
+                    />
+                </div>
+            )}
+
+            {/* Absolute images for non-scrolled or footer state */}
+            {(!isSticky || !isScrolled) && (
+                <div className="absolute inset-0 overflow-hidden pointer-events-none">
+                    <div className="sticky top-0 w-full h-full">
+                        <Image
+                            alt="Search Page"
+                            className="absolute h-[800px] w-[1500px] right-0 bottom-0"
+                            style={{
+                                color: 'transparent'
+                            }}
+                            src="/graphics/searchpage.svg"
+                            width={0}
+                            height={0}
+                        />
+                        <Image
+                            alt="Search Page"
+                            className="absolute h-[700px] w-[1350px] right-0 bottom-5"
+                            style={{
+                                color: 'transparent',
+                            }}
+                            src="/graphics/searchpageicon.svg"
+                            width={0}
+                            height={0}
+                        />
+                    </div>
+                </div>
+            )}
+            {(!isScrolled && isAdvancedSearchEnabled) && (
+                <div className="absolute inset-0 overflow-hidden pointer-events-none">
+                    <div className="fixed top-0 w-full h-full">
+                        <Image
+                            alt="Search Page"
+                            className="absolute h-[800px] w-[1500px] right-0 bottom-0"
+                            style={{
+                                position: 'fixed',
+                                color: 'transparent'
+                            }}
+                            src="/graphics/searchpage.svg"
+                            width={0}
+                            height={0}
+                        />
+                        <Image
+                            alt="Search Page"
+                            className="absolute h-[700px] w-[1350px] right-0 bottom-5"
+                            style={{
+                                position: 'fixed',
+                                color: 'transparent',
+                            }}
+                            src="/graphics/searchpageicon.svg"
+                            width={0}
+                            height={0}
+                        />
+                    </div>
+                </div>
+            )}
+
+
+
+        </div>
+    );
+};
+
+export default ImageContainer;
