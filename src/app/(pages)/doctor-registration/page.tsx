a7e8648dc67d15911ca931537ac963d253adf82c
Vince Kevin Añana <152976699+mantequilla45@users.noreply.github.com>
2025-02-23 14:20:50 -0500

    pp

--- src/app/(pages)/doctor-registration/page.tsx
+++ src/app/(pages)/doctor-registration/page.tsx
@@ -83,10 +83,7 @@ const RegistrationCard = () => {
             </div>
         </div>
     );
-};
-
-// Doctor Registration Page
-const DoctorRegistration = () => {
+}; const DoctorRegistration = () => {
     return (
         <div>
             <div className="fixed h-[10vh] flex items-center justify-center w-full">
@@ -95,13 +92,38 @@ const DoctorRegistration = () => {
                 </Link>
             </div>
             <div className="flex flex-row h-[100vh]">
-                <div className="w-full bg-[#C3EFEB]">
+                <div className="w-full relative bg-[#C3EFEB] overflow-hidden">
+                    <div className="absolute w-full z-50 p-8 flex h-full items-center">
+                        <div className="flex flex-row items-center justify-between w-full pr-[25%] pl-[10%]">
+                            <div className="flex flex-col">
+                                <p className="text-[#62B6B8] text-[8vw] leading-tight">
+                                    HELLO
+                                </p>
+                                <h1 className="text-[3vw]">
+                                    Welcome to <span className="text-[#62B6B8]">BSDOC</span>
+                                </h1>
+                                <p className="text-[1.5rem]">
+                                    Your Personal Guide to Self-Care for Common Ailments
+                                </p>
+                            </div>
+                            <div>
+                                <Image
+                                    src="graphics/floating-medicine.svg"
+                                    alt="svg"
+                                    className="object-contain w-[15vw] h-[15vw]"
+                                    width={200}
+                                    height={200}
+                                />
+                            </div>
+                        </div>
+                    </div>
                     <Image
                         src="graphics/doc-register.svg"
                         alt="svg"
-                        fill
-                        objectFit="contain"
-                        className="w-full h-auto"
+                        className="absolute left-0 top-0 object-contain w-[100%] h-[122vh]"
+                        width={1200}
+                        height={1200}
+                        priority
                     />
                 </div>
                 <div className="min-w-[600px] pt-[5%] flex items-center">
