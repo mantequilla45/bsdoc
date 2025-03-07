fd0bf46b8eff398f79eeab964bb0ada891313e34
Erudi787 <elwisondenampo@gmail.com>
2025-03-07 21:49:31 +0800

    temp fix: hardcoded fix for url redirect

--- src/services/Auth/auth.ts
+++ src/services/Auth/auth.ts
@@ -1,21 +1,21 @@
 import { supabase } from "@/lib/supabaseClient";
 
-const getDomain = () => {
-    // When running in the browser
-    if (typeof window !== 'undefined') {
-      return window.location.origin;
-    }
-    // Server-side fallback (this won't be perfect but helps)
-    return process.env.NODE_ENV === 'production'
-      ? 'https://bsdoc-project.vercel.app'
-      : 'http://localhost:3000';
-  };
+// const getDomain = () => {
+//     // When running in the browser
+//     if (typeof window !== 'undefined') {
+//       return window.location.origin;
+//     }
+//     // Server-side fallback (this won't be perfect but helps)
+//     return process.env.NODE_ENV === 'production'
+//       ? 'https://bsdoc-project.vercel.app'
+//       : 'http://localhost:3000';
+//   };
   
-  // Notice HTTP not HTTPS for localhost (matching your error)
-  const getRedirectTo = () => {
-    const domain = getDomain();
-    return `${domain}/auth/callback`;
-  };
+//   // Notice HTTP not HTTPS for localhost (matching your error)
+//   const getRedirectTo = () => {
+//     const domain = getDomain();
+//     return `${domain}/auth/callback`;
+//   };
 
 export async function signUpWithEmail(email:string, password: string) {
     const { data, error } = await supabase.auth.signUp({
@@ -40,7 +40,8 @@ export async function singInWithEmail(email: string, password: string) {
 export async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google', 
-        options: { redirectTo: getRedirectTo() }
+        //options: { redirectTo: getRedirectTo() }
+        options: { redirectTo: 'https://bsdoc-project.vercel.app/auth/callback'}
     });
 
     if (error) throw error;
