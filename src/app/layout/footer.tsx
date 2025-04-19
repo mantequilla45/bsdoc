76bddcaa1dd031ccc5a799784c20ea18fe4d973d
Theo <mrpiggyroz@gmail.com>
2025-04-19 04:52:44 +0000

    Updates

--- src/app/layout/footer.tsx
+++ src/app/layout/footer.tsx
@@ -3,7 +3,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-b from-[#018487] to-[#014C4E] py-8 md:py-16 flex flex-col">
+    <footer className="bg-gradient-to-b from-[#018487] to-[#014C4E] py-4 md:py-8">
       <div className="w-full max-w-[1300px] mx-auto px-4 sm:px-6 md:px-16">
 
         <div className="flex flex-col gap-3 md:gap-6 text-white">
@@ -15,19 +15,30 @@ const Footer = () => {
           </h3>
         </div>
 
-        <div className="font-semibold text-black my-8 md:my-16 flex justify-center md:justify-start">
-          <a className="py-3 md:py-5 px-6 sm:px-8 md:px-16 lg:px-[100px] rounded-xl bg-[#ED5050] text-white text-sm md:text-base hover:bg-[#d64646] transition-colors duration-200 cursor-pointer active:scale-95">
+        <div className="font-semibold text-black my-8 md:my-7 flex justify-center md:justify-start">
+          <a 
+            href="/appointment-page"
+            className="py-3 md:py-5 px-6 sm:px-8 md:px-16 lg:px-[100px] rounded-xl bg-[#ED5050] text-white text-sm md:text-base hover:bg-[#d64646] transition-colors duration-200 cursor-pointer active:scale-95">
             BOOK
           </a>
         </div>
 
         <div className="flex flex-col sm:flex-row items-center gap-6 sm:gap-0 sm:justify-between mb-6 sm:mb-0">
-          <a
-            href="/about-us"
-            className="text-white hover:underline text-sm md:text-base"
-          >
-            About Us
-          </a>
+          <div className="flex flex-col gap-3">
+            <a
+              href="/contact-us"
+              className="text-white hover:underline text-sm md:text-base"
+            >
+              Contact Us
+            </a>
+
+            <a
+              href="/about-us"
+              className="text-white hover:underline text-sm md:text-base"
+            >
+              About Us
+            </a>
+          </div>
 
           <div className="flex flex-row gap-5 text-white">
             <FaFacebook className="w-6 h-6 md:w-[30px] md:h-[30px] hover:opacity-80 cursor-pointer" />
